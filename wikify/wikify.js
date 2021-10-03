3ab61355cecb91442c539a872af5306f5d9d56f1
CharlieS1103 <someonelikeswindows@gmail.com>
2021-10-03 15:29:08 -0400

    Add more fallbacks

--- wikify/wikify.js
+++ wikify/wikify.js
@@ -23,40 +23,49 @@
         const uri = rawUri.split(":")[2]
         const artistName = await CosmosAsync.get(`https://api.spotify.com/v1/artists/${uri}`)
         const artistNameTrimmed = (artistName.name).replace(/\s/g, "%20");
-
+    
        if(artistName != null){
+           try{
            const wikiInfo = await CosmosAsync.get(`https://en.wikipedia.org/w/api.php?action=query&format=json&prop=extracts%7Cdescription&titles=${artistNameTrimmed}`)
            
            //https://en.wikipedia.org/w/api.php?action=query&format=json&uselang=en&list=search&srsearch=${artistNameTrimmed}
   
            const wikiInfoArr = wikiInfo.query.pages
-      
-           const page = Object.values(wikiInfoArr)[0];
-
-           var styles = `
+      console.log(wikiInfoArr)
+      console.log(Object.values(wikiInfoArr))
+            const page = Object.values(wikiInfoArr)[0];
+               var styles = `
     body > generic-modal > div > div {
     background-color: beige;
     color: black
 }
 `
 
-           var styleSheet = document.createElement("style")
-           styleSheet.type = "text/css"
-           styleSheet.innerText = styles
-           document.body.appendChild(styleSheet)
-if(page.extract != null){
-    Spicetify.PopupModal.display({
-        title: "WikiFy",
-        content: page.extract
+               var styleSheet = document.createElement("style")
+               styleSheet.type = "text/css"
+               styleSheet.innerText = styles
+               document.body.appendChild(styleSheet)
+               console.log(page.extract)
+               if (page.extract != null) {
+                   Spicetify.PopupModal.display({
+                       title: "WikiFy",
+                       content: page.extract
 
-    });
-}else{
-    Spicetify.PopupModal.display({
-        title: "Error",
-        content: "Selected artist does not have a WikiPedia page, Sorry."
+                   });
+               } else {
+                   Spicetify.PopupModal.display({
+                       title: "Error",
+                       content: "Selected artist does not have a WikiPedia page, Sorry."
 
-    });
-}
+                   });
+               }
+           }catch{
+Spicetify.PopupModal.display({
+    title: "Error",
+    content: "Request failed",
+})
+           }
+    
            
         
     }
