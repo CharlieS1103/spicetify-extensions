40a80a49388562d7fb1fea15a7e9a0440a068c13
TheGeeKing <58857539+TheGeeKing@users.noreply.github.com>
2021-11-13 23:26:08 +0000

    final commit
    FINALLY

--- wikify/wikify.js
+++ wikify/wikify.js
@@ -1,8 +1,8 @@
 //@ts-check
 
-// NAME: SongStats
+// NAME: Wikify
 // AUTHOR: CharlieS1103
-// DESCRIPTION: View a songs stats, such as dancability and acousticness.
+// DESCRIPTION: an artists wikipedia page to learn more about them
 
 /// <reference path="../globals.d.ts" />
 (function WikiFy() {
