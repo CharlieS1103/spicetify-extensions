aa2cfc4643a1718dfa40eddd07b254f29f43ce7c
CharlieS1103 <someonelikeswindows@gmail.com>
2022-01-08 19:45:10 -0500

    Block some more ads

--- adblock/adblock.js
+++ adblock/adblock.js
@@ -17,7 +17,7 @@
 
     styleSheet.innerHTML =
      `
-    .MnW5SczTcbdFHxLZ_Z8j, .WiPggcPDzbwGxoxwLWFf, .ReyA3uE3K7oEz7PTTnAn, .main-leaderboardComponent-container {
+    .MnW5SczTcbdFHxLZ_Z8j, .WiPggcPDzbwGxoxwLWFf, .ReyA3uE3K7oEz7PTTnAn, .main-leaderboardComponent-container, .sponsor-container {
     display: none !important;
     }
     `
