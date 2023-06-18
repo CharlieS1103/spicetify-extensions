485fff99fa687bc00c85302e596d129104b82d0f
notMrpod <darsh.pod@gmail.com>
2023-06-18 15:03:11 -0400

    Restore Adblock

--- adblock/adblock.js
+++ adblock/adblock.js
@@ -7,52 +7,40 @@
 /// <reference path="../../spicetify-cli/globals.d.ts" />
 
 (function adblock() {
-    // Check if the Spicetify Platform object is available
     const { Platform } = Spicetify;
     if (!Platform) {
-        // If not available, wait and retry after 300 milliseconds
-        setTimeout(adblock, 300);
-        return;
+        setTimeout(adblock, 300)
+        return
     }
 
-    // Create a new style sheet element to hide ads
-    const styleSheet = document.createElement("style");
+    const styleSheet = document.createElement("style")
 
     styleSheet.innerHTML =
         `
-    .MnW5SczTcbdFHxLZ_Z8j, .WiPggcPDzbwGxoxwLWFf, .ReyA3uE3K7oEz7PTTnAn, .main-leaderboardComponent-container, .sponsor-container, a.link-subtle.main-navBar-navBarLink.GKnnhbExo0U9l7Jz2rdc, button[title="Upgrade to Premium"], button[aria-label="Upgrade to Premium"], .main-contextMenu-menuItem a[href^="https://www.spotify.com/premium/"] {
-        display: none !important;
+    .MnW5SczTcbdFHxLZ_Z8j, .WiPggcPDzbwGxoxwLWFf, .ReyA3uE3K7oEz7PTTnAn, .main-leaderboardComponent-container, .sponsor-container, a.link-subtle.main-navBar-navBarLink.GKnnhbExo0U9l7Jz2rdc, button[title="Upgrade to Premium"], button[aria-label="Upgrade to Premium"], .main-topBar-UpgradeButton, .main-contextMenu-menuItem a[href^="https://www.spotify.com/premium/"] {
+    display: none !important;
     }
-    `;
+    `
     document.body.appendChild(styleSheet);
-
-    // Function to delay ads by adjusting stream time
-    function delayAds() {
-        console.log("Ads delayed: Adblock.js");
-        Spicetify.Platform.AdManagers.audio.audioApi.cosmosConnector.increaseStreamTime(-100000000000);
-        Spicetify.Platform.AdManagers.billboard.billboardApi.cosmosConnector.increaseStreamTime(-100000000000);
-    }
-    setInterval(delayAds, 720 * 10000); // Run the delayAds function every 7200 seconds (2 hours)
-
-    // Override the displayBillboard function to handle ad removal
+    
+    delayAds();
+    
     const billboard = Spicetify.Platform.AdManagers.billboard.displayBillboard;
-    Spicetify.Platform.AdManagers.billboard.displayBillboard = function () {
-        Spicetify.Platform.AdManagers.billboard.finish();
+    Spicetify.Platform.AdManagers.billboard.displayBillboard = function (arguments) {
+        Spicetify.Platform.AdManagers.billboard.finish()
         // hook before call
-        const ret = billboard.apply(this, arguments);
+        var ret = billboard.apply(this, arguments);
         // hook after call
-
-        // Use MutationObserver to detect the appearance of the billboard ad and remove it
+        Spicetify.Platform.AdManagers.billboard.finish()
         const observer = new MutationObserver((mutations, obs) => {
             const billboardAd = document.getElementById('view-billboard-ad');
             if (billboardAd) {
-                Spicetify.Platform.AdManagers.billboard.finish();
+                Spicetify.Platform.AdManagers.billboard.finish()
                 obs.disconnect();
                 return;
             }
         });
 
-        // Observe mutations in the document subtree
         observer.observe(document, {
             childList: true,
             subtree: true
@@ -60,26 +48,31 @@
         return ret;
     };
 
-    // Function to disable Esperanto ads by modifying product state
-    async function disableEsperantoAds() {
-        // Check if the necessary Spicetify Platform objects are available
+    async function delayAds() {
+        await Spicetify.Platform.UserAPI._product_state.putOverridesValues({ pairs: { ads: "0", catalogue: "premium", product: "premium", type: "premium" } });
+        
+        // Keeping these as safety measures
+        Spicetify.Platform.AdManagers.audio.audioApi.cosmosConnector.increaseStreamTime(-100000000000);
+        Spicetify.Platform.AdManagers.billboard.billboardApi.cosmosConnector.increaseStreamTime(-100000000000);
+        
+        await Spicetify.Platform.AdManagers.audio.disable();
+        await Spicetify.Platform.AdManagers.billboard.disable();
+        await Spicetify.Platform.AdManagers.leaderboard.disableLeaderboard();
+        await Spicetify.Platform.AdManagers.sponsoredPlaylist.disable();
+        
+        console.log("[Adblock] Ads disabled", Spicetify.Platform.AdManagers);
+    };
+
+    setInterval(delayAds, 30 * 10000);
+    
+    (async function disableEsperantoAds() {
         if (!Spicetify.Platform?.UserAPI?._product_state) {
-            // If not available, wait and retry after 300 milliseconds
             setTimeout(disableEsperantoAds, 300);
             return;
         }
-
-        // Modify the product state to disable Esperanto ads
         await Spicetify.Platform.UserAPI._product_state.putOverridesValues({ pairs: { ads: "0", catalogue: "premium", product: "premium", type: "premium" } });
-
-        // Log the disabled ads information
-        Spicetify.Platform.UserAPI._product_state.subValues({}, ({ pairs }) => {
-            console.log("Disabled Esperanto ads", pairs);
-            // Reconfirm the disabled ads
-            Spicetify.Platform.UserAPI._product_state.putOverridesValues({ pairs: { ads: "0", catalogue: "premium", product: "premium", type: "premium" } });
+        Spicetify.Platform.UserAPI._product_state.subValues({ keys: ["ads"] }, () => {
+            delayAds();
         });
-    }
-
-    // Call the disableEsperantoAds function to disable Esperanto ads
-    disableEsperantoAds();
-})();
+    })();
+})()
